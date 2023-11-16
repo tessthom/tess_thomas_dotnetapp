5601ce2d31f183ed6256e6436d05e6cb69854340
Tess Thomas <104388884+tessthom@users.noreply.github.com>
2023-11-16 18:16:50 -0500

    Add placeholder FAQ data to FaqData.cs, edit Help controller to use it.

--- Areas/Help/Controllers/HomeController.cs
+++ Areas/Help/Controllers/HomeController.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 [Area("Help")]
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    public IActionResult Index(string topic, string category)
     {
+        // Get all FAQs from FaqData.cs
+        var faqs = FaqData.AllFaqs;
+
+        // Store FAQs in ViewData
+        ViewData["faqs"] = faqs;
+
         return View();
     }
 }
---
+++ FaqData.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic; 
+
+public static class FaqData
+{
+  public static List<Dictionary<string, string>> AllFaqs = new List<Dictionary<string, string>>
+  {
+    new Dictionary<string, string>
+    {
+      { "Question", "Question 1" },
+      { "Answer", "Answer 1" },
+      { "Topic", "Topic 1" },
+      { "Category", "Category 1" }
+    },
+    new Dictionary<string, string>
+    {
+      { "Question", "Question 2" },
+      { "Answer", "Answer 2" },
+      { "Topic", "Topic 2" },
+      { "Category", "Category 2" }
+    },
+    new Dictionary<string, string>
+    {
+      { "Question", "Question 3" },
+      { "Answer", "Answer 3" },
+      { "Topic", "Topic 3" },
+      { "Category", "Category 3" }
+    },
+    new Dictionary<string, string>
+    {
+      { "Question", "Question 4" },
+      { "Answer", "Answer 4" },
+      { "Topic", "Topic 4" },
+      { "Category", "Category 4" }
+    },
+    new Dictionary<string, string>
+    {
+      { "Question", "Question 5" },
+      { "Answer", "Answer 5" },
+      { "Topic", "Topic 5" },
+      { "Category", "Category 5" }
+    }
+  };
+}
